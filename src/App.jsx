09d21f8b85c9a532468980cb6447f2f69ffc9e7e
asharglobalweb <ashar@globalweb.ae>
2025-11-13 13:42:21 +0400

    Ashar: Lazy load added in app.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import { lazy, Suspense } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './Components/Footer';
-import HomePage from './pages/HomePage';
-import School from './pages/School';
-import Transfers from './pages/Transfers';
-import Shuttles from './pages/Shuttles';
-import Coorporate from './pages/Coorporate';
-import AboutUs from './pages/AboutUs';
-import ScrollToTop from './Components/ScrollToTop';
-const Navbar = lazy(() => import('./components/Navbar'));
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./Components/ScrollToTop";
+const Navbar = lazy(() => import("./Components/Navbar"));
+const Footer = lazy(() => import("./Components/Footer"));
+const School = lazy(() => import("./pages/School"));
+const Coorporate = lazy(() => import("./pages/Coorporate"));
+const Transfers = lazy(() => import("./pages/Transfers"));
+const Shuttles = lazy(() => import("./pages/Shuttles"));
+const AboutUs = lazy(() => import("./pages/AboutUs"));
+const HomePage = lazy(() => import("./pages/HomePage"));
 
 function App() {
-
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Suspense >
+      <Suspense>
         <Navbar />
         <main className="pt-20">
           <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/school-trips' element={<School />} />
-            <Route path='/transfers' element={<Transfers />} />
-            <Route path='/Shuttles' element={<Shuttles />} />
-            <Route path='/corporate-events' element={<Coorporate />} />
-            <Route path='/about' element={<AboutUs />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/school-trips" element={<School />} />
+            <Route path="/transfers" element={<Transfers />} />
+            <Route path="/Shuttles" element={<Shuttles />} />
+            <Route path="/corporate-events" element={<Coorporate />} />
+            <Route path="/about" element={<AboutUs />} />
           </Routes>
         </main>
         <Footer />
       </Suspense>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
